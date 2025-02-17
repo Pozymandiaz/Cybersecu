47219149c2eca06b311ed99d2762ef6a64013e86
Pozymandiaz <lucasverstraete01@gmail.com>
2025-02-17 14:09:00 +0100

    début proxy

---
+++ proxy.py
@@ -0,0 +1,26 @@
+import sys
+import socket
+import threading
+
+
+#The list comprehension gives a printable character representation of the first 256 integers.
+HEX_FILTER = ''.join(
+    [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+
+    results = list()
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c):02X}' for c in word])
+        hexwidth = length*3
+        results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
